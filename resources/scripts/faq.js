520c5a793eb4d5302df1cc06e0e434d0436570e0
Batman <emismith90@gmail.com>
2016-08-20 18:15:41 +0700

    add faq

---
+++ resources/scripts/faq.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    expander();
+});
+
+function expander() {
+    var expanders = document.querySelectorAll('.expander');
+    Array.prototype.forEach.call(expanders, init$2);
+};
+
+function init$2(expander) {
+    $(expander.querySelector('.expander__toggle')).on('click', toggle);
+}
+
+function toggle(e) {
+    e.preventDefault();
+    var toggle = this;
+    var expander = $(toggle).closest('.expander');
+    expander.toggleClass('expander--open');
+    if (expander.hasClass('expander--open')) {
+        expander.find('.expander__content').attr('tabindex', -1).focus();
+    }
+}
